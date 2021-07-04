04d8cab96dcd25b343bbf44fe8a8f515652f10cb
Adrian Furmanek <adrian.furm@gmail.com>
2021-07-04 23:57:26 +0200

    Rocket Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessFly();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ProcessFly()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }      
+    }
+    void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
     }
 }
